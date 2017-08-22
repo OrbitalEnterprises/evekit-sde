966f5f21d9ed94b8ce7ec1decc560163d648ddc4
Mark Astley <deadlybulb@orbital.enterprises>
2017-08-22 19:54:45 -0400

    fix tests due to data problem

--- src/test/java/enterprises/orbital/evekit/sde/invtests/TestInvUniqueName.java
+++ src/test/java/enterprises/orbital/evekit/sde/invtests/TestInvUniqueName.java
@@ -21,7 +21,7 @@ public class TestInvUniqueName extends TestSetup {
       contid += next.size();
       next = InvUniqueName.access(contid, maxresults, all, all, all);
     }
-    Assert.assertEquals(342013, contid);
+    Assert.assertEquals(365433, contid);
   }
 
   @Test
